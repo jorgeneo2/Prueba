54c22d3ba23502c07e448b5d21c5d9e9fd32d7f1
enieto2018 <e.nieto@utp.edu.co>
2018-08-19 22:21:39 -0500

    mis test

--- Primer_Programa/src/Test/Monto_EscritoTest.java
+++ Primer_Programa/src/Test/Monto_EscritoTest.java
@@ -12,9 +12,38 @@ class Monto_EscritoTest extends Monto_Escrito {
 	@Test
 	void testUnidades() {
 		Monto_Escrito unidades = new Monto_Escrito();
-		String resultadoEsperado = "cero";
-		String resultadoObtenido = unidades.calcularUnidad(0);
+		String resultadoEsperado = "uno";
+		String resultadoObtenido = unidades.calcularUnidad(1);
 		assertEquals(resultadoEsperado, resultadoObtenido);
 	}
 
+	@Test
+	void testEspecial() {
+		Monto_Escrito especial = new Monto_Escrito();
+		String resultadoEsperado = "once";
+		String resultadoObtenido = especial.calcularEspecial(11);
+		assertEquals(resultadoEsperado, resultadoObtenido);
+	}
+	
+	@Test
+	void testDecenas() {
+		Monto_Escrito decenas = new Monto_Escrito();
+		String resultadoEsperado = "veinte";
+		String resultadoObtenido = decenas.calcularDecenas(20);
+		assertEquals(resultadoEsperado, resultadoObtenido);
+	}
+	@Test
+	void testCentenas() {
+		Monto_Escrito centenas = new Monto_Escrito();
+		String resultadoEsperado = "doscientos";
+		String resultadoObtenido = centenas.calcularCentenas(2);
+		assertEquals(resultadoEsperado, resultadoObtenido);
+	}
+	@Test
+	void testunidMil() {
+		Monto_Escrito unimil = new Monto_Escrito();
+		String resultadoEsperado = "dos mil";
+		String resultadoObtenido = unimil.calcularunidMil(2000);
+		assertEquals(resultadoEsperado, resultadoObtenido);
+	}
 }
--- Primer_Programa/src/paquete/Monto_Escrito.java
+++ Primer_Programa/src/paquete/Monto_Escrito.java
@@ -1,15 +1,108 @@
 package paquete;
+
 import java.util.Scanner;
 
 public class Monto_Escrito {
-	String unidades[] = 
-        {"cero", "uno", "dos" ,"tres" ,"cuatro" ,"cinco" ,
-            "seis" ,"siete" ,"ocho" ,"nueve"};
 	
-        public String calcularUnidad(int num){
-        	 {
-			return unidades[num];
+	String letra;
+	String resultado;
+	String resultado1;
+	String resultado2;
+	String resultado3;
+	String resultado4;
+	
 
-        	}
-      }
-}
+	String unidades[] = { "cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve" };
+
+	String especial[] = { "once", "doce", "trece", "catorce", "quince", "dieciseis", "diecisiete", "dieciocho",
+			"diecinueve" };
+
+	String decenas[] = { "diez", "veinte", "treinta", "cuarenta", "cincuenta", "sesenta", "setenta", "ochenta",
+			"noventa" };
+
+	 String centenas[] =
+	        {"cien","doscientos","trescientos","cuatroscientos","quinientos",
+	        	"seiscientos","setescientos","ochoscientos","novescientos"};
+	 
+    String unidMil[]=
+    {"mil", "dos mil","tres mil", "cuatro mil","cinco mil","seis mil",
+    	"siete mil","ocho mil", "nueve mil", "diez mil"};
+	        
+    public String calcularUnidad(int num) {
+		if(num<10) {
+			resultado=unidades[num];
+		}
+		return resultado;
+	}
+
+	public String calcularEspecial(int num) {
+		if(num > 10 && num<20) {
+			resultado1=especial[num - 11];
+		}
+			return resultado1;
+	}
+	public String calcularDecenas(int num) {
+		if(num<100){
+            int unid = num % 10;
+            int dec = num/10;
+            if(unid == 0)
+            	resultado2=(decenas[dec-1]);                
+            else
+            	resultado2=(decenas[dec-1]
+                                       + " y " + unidades[unid]);
+		}
+			return resultado2;
+
+		}
+
+	
+	public String calcularCentenas(int num) {
+		if(num<1000){
+            int unid = num % 10;
+            int dec = (num/10)%10;
+            int cent=num/10/10;
+            if(unid == 0 && dec==0)
+            	resultado3=(centenas[cent-1]);  
+            if(dec==0)
+            	resultado3=(centenas[cent-1] +" "+ unidades[unid]);   
+            else if(dec == 1 && unid !=0)
+            	resultado3=(centenas[cent-1] +" "+ especial[unid-1]);   
+            else if(dec == 1 && unid ==0)
+            	resultado3=(centenas[cent-1] +" "+ decenas[(dec-1)]);
+            else
+            	resultado3=(centenas[cent-1] +" "+ decenas[(dec-1)]
+                                       + " y " + unidades[unid]);
+		}
+			return resultado3;
+
+		}
+
+
+	public String calcularunidMil(int num) {
+		if(num<10000){
+        	int unid = num % 10;
+            int dec = (num/10)%10;
+            int cent=num/10/10%10;
+            int unidmil=num/10/10/10%10;
+            String textoNum= unidMil[unidmil-1];
+            if(cent>0) {
+                textoNum=textoNum+" "+ centenas[cent-1];
+            }
+            if(dec == 1 && unid > 0)
+                    textoNum=textoNum+" "+ especial[unid-1];
+            else if(dec > 1){
+                textoNum=textoNum+" "+ decenas[(dec-1)];
+                 if(unid>0)
+                     textoNum=textoNum+" y " + unidades[unid];
+            }else if(unid>0){
+                     textoNum=textoNum+" " + unidades[unid];
+            }
+		
+            resultado4=(textoNum); 
+ 
+		}
+			return resultado4;
+
+		}
+
+	}
